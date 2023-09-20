ec5d7675498fc2d129cc28949f0847a6f96f1350
denizyunsel <107466974+denizyunsel@users.noreply.github.com>
2023-09-20 13:18:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,12 @@ import cloudToMesh as ctm
 import meshManager as HaF
 import time
 
-#Take images from camera then convert it to mesh
-#pcd="C:/Users/stj.dyunsel/Desktop/MeshProject/dataset12.ply"
-#ctm.get_mesh_from_pcd(pcd)
+# #Take images from camera then convert it to mesh
+# ctm.convertMesh()
 
 #Load the 3D mesh
 i=1
-for i in range(11,12):
+for i in range(1,12):
     dataset_name = f"dataset{i}.ply"
 
     mesh = o3d.io.read_triangle_mesh(dataset_name)
@@ -39,7 +38,7 @@ for i in range(11,12):
 
     #Clustering
     start_time = time.time()
-    obj.apply_clustering(mesh,dataset_name)
+    obj.apply_DBSCAN(mesh,dataset_name)
     end_time = time.time()
 
     elapsed_time = end_time - start_time
